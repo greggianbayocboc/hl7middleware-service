fd4bd6599ac5cc725ff71b6c7b87ebd1b2eee76e
crisnil <cacuyado@gmail.com>
2019-04-16 15:17:59 +0800

    add room details

---
+++ src/main/resources/public/build/precache-manifest.edffe1b349aa24946484467065e85316.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "aa600602c21accd2ae1c",
+    "url": "/static/js/main.aa600602.chunk.js"
+  },
+  {
+    "revision": "9865ef5823f8f930a80f",
+    "url": "/static/js/1.9865ef58.chunk.js"
+  },
+  {
+    "revision": "aa600602c21accd2ae1c",
+    "url": "/static/css/main.3fc7e704.chunk.css"
+  },
+  {
+    "revision": "6fb9a2b307fe6bb2cc9f5668629450c5",
+    "url": "/index.html"
+  }
+];
